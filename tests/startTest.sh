f9ee0d1a7b64191d4b8f507ecf5f98c9b9bf85b0
Alice <peo_alichtenthaler@uolinc.com>
2017-08-30 11:55:27 -0300

    merging

--- tests/startTest.sh
+++ tests/startTest.sh
@@ -48,4 +48,4 @@ docker exec testMycenae /bin/sh -c "echo $cassandra3 cassandra3 >> /etc/hosts"
 docker exec testMycenae /bin/sh -c "echo $elastic elastic >> /etc/hosts"
 docker exec testMycenae /bin/sh -c "echo $mycenae mycenae >> /etc/hosts"
 
-docker exec testMycenae go test -timeout 20m -v ../tests/
+# docker exec testMycenae go test -timeout 20m -v ../tests/
