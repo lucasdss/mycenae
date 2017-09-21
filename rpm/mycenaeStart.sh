0a3fac87320cbdecd552e6acdff5bbf5114a7199
Daniel Espinha <despinha@uolinc.com>
2017-09-21 19:23:26 -0300

    fix start script

--- rpm/mycenaeStart.sh
+++ rpm/mycenaeStart.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 sleep 30
-/opt/mycenae/bin/mycenae -config=/etc/opt/mycenae/mycenae.toml 2>&1 | cronolog -S /var/log/mycenae/mycenae.log "/var/log/mycenae/mycenae.log-%Y-%m-%d_%H:%M:%S" &
+/opt/mycenae/bin/mycenae -config=/etc/opt/mycenae/mycenae.toml 2>&1 | cronolog -S /var/log/mycenae/mycenae.log "/var/log/mycenae/mycenae.log-%Y-%m-%d" &
